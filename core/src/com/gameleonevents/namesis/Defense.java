e58edb149f25e9744c22021939e414876744e15c
Pierre Nicolai <pierre.nicolai.06@gmail.com>
2016-02-22 15:26:22 +0100

    Adding base for defense mini-game
    Click listeners are implemented, base can be reused for attack mini
    game.

---
+++ core/src/com/gameleonevents/namesis/Defense.java
@@ -0,0 +1,166 @@
+package maje.gameleon.hunterd;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import javax.xml.soap.Text;
+
+/**
+ * Created by Pierre on 17/02/2016.
+ */
+
+
+public class Defense extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+
+    private TextureAtlas applicationAtlas;
+    private Skin applicationSkin;
+    private Stage stage;
+
+    private Sprite background;
+
+    private GameState gameState;
+    private OrthographicCamera camera;
+
+    private ImageButton blueButton;
+    private ImageButton yellowButton;
+    private ImageButton greenButton;
+    private ImageButton purpleButton;
+
+
+    @Override
+    public void create()
+    {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        batch = new SpriteBatch();
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        applicationAtlas = new TextureAtlas("data/buttons.pack");
+        applicationSkin = new Skin();
+        applicationSkin.addRegions(applicationAtlas);
+
+        background = new Sprite(new Texture("data/defense_background.png"));
+
+        InitializeButtons();
+
+        gameState = GameState.INGAME;
+    }
+
+    @Override
+    public void render()
+    {
+        if(gameState == GameState.INGAME)
+        {
+            Gdx.gl.glClearColor(1, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+            batch.begin();
+            batch.draw(background, 0, 0, 800, 480);
+            batch.end();
+            batch.begin();
+            stage.draw();
+            batch.end();
+        }
+    }
+
+    public void InitializeButtons()
+    {
+        //Initialize blue button
+        ImageButton.ImageButtonStyle blueButtonStyle = new ImageButton.ImageButtonStyle();
+        blueButtonStyle.imageUp = applicationSkin.getDrawable("blue_orb");
+
+        blueButton = new ImageButton(blueButtonStyle);
+        blueButton.setPosition(620, 80);
+
+        blueButton.addListener(new InputListener(){
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+                System.out.println("Click sur blue button");
+                return true;
+            }
+        });
+
+        stage.addActor(blueButton);
+
+        //Initialize yellow button
+        ImageButton.ImageButtonStyle yellowButtonStyle = new ImageButton.ImageButtonStyle();
+        yellowButtonStyle.imageUp = applicationSkin.getDrawable("yellow_orb");
+
+        yellowButton = new ImageButton(yellowButtonStyle);
+        yellowButton.setPosition(20, 80);
+
+        yellowButton.addListener(new InputListener(){
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+                System.out.println("Click sur yellow button");
+                return true;
+            }
+        });
+
+        stage.addActor(yellowButton);
+
+        //Initialize green button
+        ImageButton.ImageButtonStyle greenButtonStyle = new ImageButton.ImageButtonStyle();
+        greenButtonStyle.imageUp = applicationSkin.getDrawable("green_orb");
+
+        greenButton = new ImageButton(greenButtonStyle);
+        greenButton.setPosition(190, 20);
+
+        greenButton.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Click sur green button");
+                return true;
+            }
+        });
+
+        stage.addActor(greenButton);
+
+        //Initialize green button
+        ImageButton.ImageButtonStyle purpleButtonStyle = new ImageButton.ImageButtonStyle();
+        purpleButtonStyle.imageUp = applicationSkin.getDrawable("purple_orb");
+
+        purpleButton = new ImageButton(purpleButtonStyle);
+        purpleButton.setPosition(470, 20);
+
+        purpleButton.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Click sur purple button");
+                return true;
+            }
+        });
+
+        stage.addActor(purpleButton);
+    }
+
+    public void NotifyWin()
+    {
+        gameState = GameState.STOPPED;
+    }
+
+    public void NotifyLoose()
+    {
+        gameState = GameState.STOPPED;
+    }
+}
